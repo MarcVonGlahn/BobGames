2b6ac5f1d599ac5eb3ecf66327ba465e19317240
Marc von Glahn <marc.von.glahn@gmail.com>
2024-04-20 23:33:22 +0200

    Added engine sound to the motor prefab

---
+++ DDSTSMTBA/Assets/Scripts/CustomWwise/Car_EngineSound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_EngineSound : MonoBehaviour
+{
+    public bool isNPC = false;
+    public AK.Wwise.RTPC enginePitchRTPC;
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        enginePitchRTPC.SetValue(gameObject, _rb.velocity.magnitude);
+    }
+}
