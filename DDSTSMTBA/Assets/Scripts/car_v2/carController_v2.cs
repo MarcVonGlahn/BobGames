de4fc480df05655d1b47eb459cb17645bee0c97b
Raestkjoot <gunnarmagnum@gmail.com>
2024-04-21 04:59:05 +0200

    camera + attack anims added to car_v2

--- DDSTSMTBA/Assets/Scripts/car_v2/carController_v2.cs
+++ DDSTSMTBA/Assets/Scripts/car_v2/carController_v2.cs
@@ -1,15 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class carController_v2 : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float motorTorque = 2000;
     [SerializeField] private float maxSpeed = 20;
     [SerializeField] private float steeringRange = 30;
     [SerializeField] private float steeringRangeAtMaxSpeed = 10;
 
+    [Header("Attack")]
+    [SerializeField] private float attackChargeTime = 0.45f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject attackHitbox;
+
     private float brakeTorque = 2000;
     private wheelControl_v2[] wheels;
     private Rigidbody rigidBody;
+
+    private bool isAttacking = false;
+    private bool isChargingAttack = false;
+    private float attackChargeStartTime = -1.0f;
 
     private void Start()
     {
@@ -19,10 +30,20 @@ public class carController_v2 : MonoBehaviour
 
     private void Update()
     {
+        // Movement
         float vInput = Input.GetAxis("Vertical");
         float hInput = Input.GetAxis("Horizontal");
-
         Move(vInput, hInput);
+
+        // Attacking
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ChargeAttack();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            TryAttack();
+        }
     }
 
     private void Move(float vInput, float hInput)
@@ -53,6 +74,65 @@ public class carController_v2 : MonoBehaviour
                 wheel.WheelCollider.brakeTorque = Mathf.Abs(vInput) * brakeTorque;
                 wheel.WheelCollider.motorTorque = 0;
             }
+        }
+    }
+
+    private void ChargeAttack()
+    {
+        if (!isChargingAttack)
+        {
+            attackChargeStartTime = Time.time;
+            isChargingAttack = true;
+            // TODO play charge sound
+
+            animator.SetBool("isCharging", true);
+        }
+        else if (isAttacking == true)
+        {
+            isAttacking = false;
+        }
+    }
+
+    private void TryAttack()
+    {
+        if (isAttacking)
+            return;
+
+        isAttacking = true;
+        StartCoroutine(DoAttack());
+    }
+
+    private IEnumerator DoAttack()
+    {
+        float curAttackChargeTime = Time.time - attackChargeStartTime;
+
+        while (curAttackChargeTime < attackChargeTime)
+        {
+            if (isAttacking == false)
+            {
+                yield break;
+            }
+
+            curAttackChargeTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        if (isAttacking)
+        {
+            animator.SetBool("isAttacking", true);
+            attackHitbox.SetActive(true);
+            // TODO play attack sound
+
+            yield return new WaitForSeconds(0.1f);
+
+            isAttacking = false;
+            isChargingAttack = false;
+
+            attackHitbox.SetActive(false);
+            animator.SetBool("isAttacking", false);
+            animator.SetBool("isCharging", false);
+            // TODO play idle anim 
         }
     }
 }
